a6b9741a571413e964cde0ff37126217f3ee2873
ChrisStyles99 <chrisrdz99@hotmail.com>
2020-10-28 14:51:16 -0600

    agregado el login con firebase

---
+++ src/views/Bitacora.vue
@@ -0,0 +1,69 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button></ion-back-button>
+        </ion-buttons>
+        <ion-title>Registro de Visita</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <div id="container">
+      <div v-if="visits.length < 1">
+        <p>No tienes registro de entradas</p>
+      </div>
+      <ion-card v-for="(visit, index) in visits" :key="index">
+      <ion-card-header>
+        <ion-card-title>{{visit.name}} {{visit.lastname}}</ion-card-title>
+        <ion-card-subtitle>{{visit.plate}}</ion-card-subtitle>
+      </ion-card-header>
+      <ion-card-content>
+        {{visit.date}} - {{visit.time}}
+      </ion-card-content>
+    </ion-card>
+    </div>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {IonPage, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle} from '@ionic/vue'
+import { defineComponent } from 'vue'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+export default defineComponent({
+  components: {
+    IonPage, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle
+  },
+  data() {
+    return {
+      visits: [] as any[],
+      userId: ''
+    }
+  },
+  async created() {
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        this.userId = user.uid;
+      } else {
+        this.userId = '';
+      }
+    });
+    const querySnapshot = await firebase.firestore().collection('visits').where('userId', '==', this.userId).get();
+    querySnapshot.forEach(doc => {
+      this.visits.push(doc.data());
+    });
+  },
+})
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50px;
+}
+</style>
