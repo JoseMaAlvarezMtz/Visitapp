74bebd3ffb01dd516fc84ea97288249a50ade49d
ChrisStyles99 <chrisrdz99@hotmail.com>
2020-10-28 18:25:48 -0600

    Arreglado vista bitacoras (esperemos)

--- src/views/Bitacora.vue
+++ src/views/Bitacora.vue
@@ -13,49 +13,78 @@
         <p>No tienes registro de entradas</p>
       </div>
       <ion-card v-for="(visit, index) in visits" :key="index">
-      <ion-card-header>
-        <ion-card-title>{{visit.name}} {{visit.lastname}}</ion-card-title>
-        <ion-card-subtitle>{{visit.plate}}</ion-card-subtitle>
-      </ion-card-header>
-      <ion-card-content>
-        {{visit.date}} - {{visit.time}}
-      </ion-card-content>
-    </ion-card>
+        <ion-card-header>
+          <ion-card-title>{{ visit.name }} {{ visit.lastname }}</ion-card-title>
+          <ion-card-subtitle>{{ visit.plate }}</ion-card-subtitle>
+        </ion-card-header>
+        <ion-card-content>
+          {{ visit.date }} - {{ visit.time }}
+        </ion-card-content>
+      </ion-card>
     </div>
   </ion-page>
 </template>
 
 <script lang="ts">
-import {IonPage, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle} from '@ionic/vue'
-import { defineComponent } from 'vue'
+import {
+  IonPage,
+  IonCard,
+  IonCardContent,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonTitle,
+} from '@ionic/vue';
+import { defineComponent } from 'vue';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
 export default defineComponent({
   components: {
-    IonPage, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle
+    IonPage,
+    IonCard,
+    IonCardContent,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
   },
   data() {
     return {
       visits: [] as any[],
-      userId: ''
-    }
+      userId: '',
+    };
   },
   async created() {
-    firebase.auth().onAuthStateChanged(user => {
-      if(user) {
+    await firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
         this.userId = user.uid;
       } else {
         this.userId = '';
       }
     });
-    const querySnapshot = await firebase.firestore().collection('visits').where('userId', '==', this.userId).get();
-    querySnapshot.forEach(doc => {
-      this.visits.push(doc.data());
-    });
+    try {
+      console.log(this.userId);
+      const querySnapshot = await firebase
+        .firestore()
+        .collection('visits')
+        .where('userId', '==', this.userId)
+        .get();
+      querySnapshot.forEach((doc) => {
+        this.visits.push(doc.data());
+      });
+    } catch (error) {
+      console.log(error);
+    }
   },
-})
+});
 </script>
 
 <style scoped>
